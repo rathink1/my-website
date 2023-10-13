9bc9a31310bd43bd54070061f665a396189d638c
rathink1 <rathink00400@gmail.com>
2023-10-13 13:39:31 +0200

    google verification added

--- src/site.config.ts
+++ src/site.config.ts
@@ -29,11 +29,11 @@ export const menuLinks: Array<{ title: string; path: string }> = [
 		path: "/",
 	},
 	{
-		title: "About",
-		path: "/about/",
-	},
-	{
 		title: "Blog",
 		path: "/posts/",
+	},
+	{
+		title: "About",
+		path: "/about/",
 	},
 ];
